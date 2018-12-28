b20d9aa53be6322e926692f47fe7479ce56e4f2b
Abdelrahman Afaneh <ak.afaneh@gmail.com>
2018-12-28 15:31:17 +0200

    superb
    <3

---
+++ superb/precache-manifest.26430a0dcd83507fd3c38f99c66cce19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c324f2dba94cc1e23ce",
+    "url": "/superb/static/js/runtime~main.2c324f2d.js"
+  },
+  {
+    "revision": "a7733609394db59da589",
+    "url": "/superb/static/js/main.a7733609.chunk.js"
+  },
+  {
+    "revision": "f8331e2774a50b36e6cb",
+    "url": "/superb/static/js/1.f8331e27.chunk.js"
+  },
+  {
+    "revision": "a7733609394db59da589",
+    "url": "/superb/static/css/main.d25a8165.chunk.css"
+  },
+  {
+    "revision": "7916759fad5dd916adc815879eb92ca0",
+    "url": "/superb/index.html"
+  }
+];
