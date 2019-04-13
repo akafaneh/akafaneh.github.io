e8d30b9060f919ca822f325b574e5691859b0e2d
abboud <ak.afaneh@gmail.com>
2019-04-13 17:26:13 +0200

    add soundboks

---
+++ soundboks/precache-manifest.16e4fe0c54c2ace4e28a0c7068032b41.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "81bacec3e65d9abfd9bd",
+    "url": "/soundboks/static/css/main.569c0fc5.chunk.css"
+  },
+  {
+    "revision": "81bacec3e65d9abfd9bd",
+    "url": "/soundboks/static/js/main.3b506748.chunk.js"
+  },
+  {
+    "revision": "7014389abdd46155051a",
+    "url": "/soundboks/static/js/runtime~main.32e17703.js"
+  },
+  {
+    "revision": "3abf25b3d4bf0ea6aace",
+    "url": "/soundboks/static/js/2.6cb14a38.chunk.js"
+  },
+  {
+    "revision": "7b4631dc761cb9f8b8b460d8fa1fb9c7",
+    "url": "/soundboks/static/media/Desktop_Battery-2x.7b4631dc.jpg"
+  },
+  {
+    "revision": "ec8425406d83b146ea1d2116f8742270",
+    "url": "/soundboks/static/media/Desktop_CampaignImage-2x.ec842540.jpg"
+  },
+  {
+    "revision": "0108d10341bf7eab6aecd030fa3a1ce8",
+    "url": "/soundboks/static/media/Desktop_DealImage-2x.0108d103.jpg"
+  },
+  {
+    "revision": "7842947fcde5e5cb2db29df1d02bf28f",
+    "url": "/soundboks/static/media/Desktop_Durability-2x.7842947f.jpg"
+  },
+  {
+    "revision": "9d4f7fa5c7a28719f95387c83028d720",
+    "url": "/soundboks/static/media/Desktop_Hero-2x.9d4f7fa5.jpg"
+  },
+  {
+    "revision": "da7028e8af6454685fb92d20353749c9",
+    "url": "/soundboks/static/media/Desktop_Portability-2x.da7028e8.jpg"
+  },
+  {
+    "revision": "44de9ee9a316eb60df5242e80687d6a8",
+    "url": "/soundboks/static/media/Desktop_SB2PageHero-2x.44de9ee9.jpg"
+  },
+  {
+    "revision": "935b5752713e172467408dc0669b29db",
+    "url": "/soundboks/static/media/Desktop_Volume-2x.935b5752.jpg"
+  },
+  {
+    "revision": "acbe132014d201ffe2a5996f96e909cd",
+    "url": "/soundboks/static/media/Desktop_loop_min.acbe1320.mp4"
+  },
+  {
+    "revision": "be7bb43fad63aea7a9b8b913545d7935",
+    "url": "/soundboks/static/media/Desktop_loop.be7bb43f.mp4"
+  },
+  {
+    "revision": "21bd6e2749a4a7ce0d22a859e682e25b",
+    "url": "/soundboks/static/media/GT-America-Expanded-Black.21bd6e27.ttf"
+  },
+  {
+    "revision": "fd74f9f195713866ff65794f2fcad4ab",
+    "url": "/soundboks/static/media/Desktop_Battery.fd74f9f1.jpg"
+  },
+  {
+    "revision": "392c170ef6a88c4b8c113a4ee8906a25",
+    "url": "/soundboks/static/media/Desktop_CampaignImage.392c170e.jpg"
+  },
+  {
+    "revision": "2b99619ceb57ef8d82bc6209b7a20ca2",
+    "url": "/soundboks/static/media/Desktop_DealImage.2b99619c.jpg"
+  },
+  {
+    "revision": "8ba86615b1f0f503286e5639edf69313",
+    "url": "/soundboks/static/media/Desktop_Durability.8ba86615.jpg"
+  },
+  {
+    "revision": "a9fd89b514651abc5ff4ae0ef770be0a",
+    "url": "/soundboks/static/media/Desktop_Hero.a9fd89b5.jpg"
+  },
+  {
+    "revision": "36d185c442dab2a31d20dfda1c877e1a",
+    "url": "/soundboks/static/media/Desktop_Portability.36d185c4.jpg"
+  },
+  {
+    "revision": "2e44984c33829ca06c52c96dd61b1984",
+    "url": "/soundboks/static/media/Desktop_SB2PageHero.2e44984c.jpg"
+  },
+  {
+    "revision": "97226e619981127e1c15f8029cdfbd07",
+    "url": "/soundboks/static/media/Desktop_Volume.97226e61.jpg"
+  },
+  {
+    "revision": "7d3bdc7f861f3aa265dde69b6c6d3a5b",
+    "url": "/soundboks/static/media/logo.7d3bdc7f.svg"
+  },
+  {
+    "revision": "a89c38216b4a3b63cc86329e7e555f05",
+    "url": "/soundboks/index.html"
+  }
+];
